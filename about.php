735dac2a71daa782514bc557165ead84b1b91048
VanUliK <m.vanulik@gmail.com>
2023-04-09 22:58:59 +0200

    reload

---
+++ about.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Контакты - Шаурма House</title>
+  <link rel="stylesheet" href="./style.css">
+</head>
+
+<body>
+  <?php include "header.html"; ?>
+
+  <main>
+    <section>
+      <h1 class="main-head">Контакты</h1>
+      <div class="decor"></div>
+
+      <p>Связаться с нами можно по телефону, по электронной почте или через социальные сети:</p>
+      <ul>
+        <li>Телефон: +7 (123) 456-78-90</li>
+        <li>Email: info@shaurmahouse.com</li>
+        <li>Instagram: @shaurmahouse</li>
+        <li>Facebook: /shaurmahouse</li>
+      </ul>
+      <p>Мы находимся по адресу:</p>
+      <ul>
+        <li>г. Москва, ул. Пушкина, д. 10</li>
+      </ul>
+      <p>Мы работаем ежедневно с 10:00 до 22:00. Ждем вас!</p>
+    </section>
+  </main>
+  <?php include "footer.html"; ?>
+
+</body>
+
+</html>
